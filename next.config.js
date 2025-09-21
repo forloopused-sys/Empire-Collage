e5083b5a081973525a587650018e3486a497a8f4
forloopused-sys <forloopused@gmail.com>
2025-09-21 20:47:47 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  // Your existing config, e.g., output: 'standalone' if using
+});
